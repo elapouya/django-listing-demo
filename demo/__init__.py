c6da13e0b02340a4b4df60ffb5b2a2ef494c989a
Eric Lapouyade <elapouya@gmail.com>
2020-05-31 13:32:30 +0200

    Add License file

---
+++ demo/__init__.py
@@ -0,0 +1,7 @@
+__version__='0.0.1'
+__author__ = "Eric Lapouyade"
+__copyright__ = "Copyright 2020, The Django listing demo Project"
+__credits__ = ["Eric Lapouyade"]
+__license__ = "GPLv3"
+__maintainer__ = "Eric Lapouyade"
+__status__ = "Alpha"
