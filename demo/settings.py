45280c2451918fcea3372d71c501eb680aab901b
elapouya <elapouya@gmail.com>
2023-02-04 17:06:14 +0100

    Add DEFAULT_AUTO_FIELD

--- demo/settings.py
+++ demo/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
